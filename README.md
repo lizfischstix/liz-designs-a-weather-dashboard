30dd47a6a838f5290a4ce5613735e100948aa0e5
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-02 22:53:59 -0500

    added all required curent weather data. completed README file.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # liz-designs-a-weather-dashboard
+
+## description
+ This application is a weather dashboard created to help users retrieve local weather when vacation planning.  
+ Through its creation I learned a lot about fetching and extracting relevant api data.
+
+ ## use
+ The app can be accessed at: https://lizfischstix.github.io/liz-designs-a-weather-dashboard/
+ Locations' weather information will be desplayed when the user enters the name of the city whose weather data they're looking for and click the "Go!" button.
+
+ ## license
+ MIT licence
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var curentWeather = document.getElementById('crntWea');
+
 var srchBtn = document.getElementById('srchBtn')
 var cityInput = document.getElementById('cityName');
 var API_KEY = "cdce0ff6cdae7cd27b86010906e35a95";
@@ -6,58 +7,95 @@ var WEATHER_API_KEY = "f22b30c177cf02e3cb270d3762836e86";
 var currentCityEl = document.getElementById("currentCity")
 var currentTempEl = document.getElementById("crntTemp")
 var weatherIconEl = document.getElementById("weatherIcon")
+var crntWindSpeedEl = document.getElementById("crntWind")
+var crntHumidityEl = document.getElementById("crntHumidity")
 
 
 function searchFormSubmit(event) {
     event.preventDefault();
 
-if (!cityInput){
-console.error("city not found");
-return;
-}
+    if (!cityInput) {
+        console.error("city not found");
+        return;
+    }
 
-console.log(cityInput.value);
-var cityName = cityInput.value;
-getLatAndLong(cityName)
+    console.log(cityInput.value);
+    var cityName = cityInput.value;
+    getLatAndLong(cityName)
 
 }
 
 
 //api pull framework.
 
-function getLatAndLong(cityName){
+function getLatAndLong(cityName) {
     var geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + API_KEY;
-    
+
     fetch(geoCodeURL)
-    .then(function(res){
-       return res.json();
-    })
-    .then(function(data){
-       getCurrentWeather(data[0].lat, data[0].lon);
-
-    })
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            getCurrentWeather(data[0].lat, data[0].lon);
+        })
 
 }
 
-function getCurrentWeather(lat,lon) {
-    
+function getCurrentWeather(lat, lon) {
+
     var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + API_KEY;
-   
+
     fetch(currentWeatherURL)
-    .then(function(res){
-        return res.json();
-    })
-    .then(function(data){
-        console.log(data);
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-        currentCityEl.textContent = data.name + " " + data.dt
-        weatherIconEl.src ="https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png" 
-        currentTempEl.textContent = "Temp: "+data.main.temp+ " F"
-    })
+            currentCityEl.textContent = data.name + " " + data.dt
+            weatherIconEl.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+            currentTempEl.textContent = "Temp: " + data.main.temp + " F"
+            crntWindSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " mph"
+            crntHumidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+        })
 
-}
+    function getCurrentWeather(lat, lon) {
 
+        var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + API_KEY;
 
-srchBtn.addEventListener('click', searchFormSubmit);
+        fetch(currentWeatherURL)
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
 
-//create element/append element
+                currentCityEl.textContent = data.name + " " + data.dt
+                weatherIconEl.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+                currentTempEl.textContent = "Temp: " + data.main.temp + " F"
+                crntWindSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " mph"
+                crntHumidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+            })
+    }
+
+    function getFiveDayForecast(lat, lon) {
+
+        var currentWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + API_KEY;
+
+        fetch(currentWeatherURL)
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
+
+                currentCityEl.textContent = data.name + " " + data.dt
+                weatherIconEl.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+                currentTempEl.textContent = "Temp: " + data.main.temp + " F"
+                crntWindSpeedEl.textContent = "Wind Speed: " + data.wind.speed + " mph"
+                crntHumidityEl.textContent = "Humidity: " + data.main.humidity + "%"
+            })
+    }
+ localStorage.setItem 
+    srchBtn.addEventListener('click', searchFormSubmit);
+
