92a11380ddca19f562ecadda5f8da83f9e51d861
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-02 20:24:55 -0500

    pulled most of the current weather data onto the page.  Need to pul 5day onto the page and save in local storage.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,63 @@
-var apiKey = "cdce0ff6cdae7cd27b86010906e35a95";
-var requestURL = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={"cdce0ff6cdae7cd27b86010906e35a95"}
+var curentWeather = document.getElementById('crntWea');
+var srchBtn = document.getElementById('srchBtn')
+var cityInput = document.getElementById('cityName');
+var API_KEY = "cdce0ff6cdae7cd27b86010906e35a95";
+var WEATHER_API_KEY = "f22b30c177cf02e3cb270d3762836e86";
+var currentCityEl = document.getElementById("currentCity")
+var currentTempEl = document.getElementById("crntTemp")
+var weatherIconEl = document.getElementById("weatherIcon")
 
 
-function SearchFormSubmit(event) {
-    event.PreventDefault();
+function searchFormSubmit(event) {
+    event.preventDefault();
 
-var searchFormEl = document.querySelector('#cityName').value;
-if (!cityName){
+if (!cityInput){
 console.error("city not found");
 return;
 }
 
-var queryString = //'./search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+console.log(cityInput.value);
+var cityName = cityInput.value;
+getLatAndLong(cityName)
 
 }
+
+
+//api pull framework.
+
+function getLatAndLong(cityName){
+    var geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + API_KEY;
+    
+    fetch(geoCodeURL)
+    .then(function(res){
+       return res.json();
+    })
+    .then(function(data){
+       getCurrentWeather(data[0].lat, data[0].lon);
+
+    })
+
+}
+
+function getCurrentWeather(lat,lon) {
+    
+    var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + API_KEY;
+   
+    fetch(currentWeatherURL)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        currentCityEl.textContent = data.name + " " + data.dt
+        weatherIconEl.src ="https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png" 
+        currentTempEl.textContent = "Temp: "+data.main.temp+ " F"
+    })
+
+}
+
+
+srchBtn.addEventListener('click', searchFormSubmit);
+
+//create element/append element
